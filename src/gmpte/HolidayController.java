7078e597d555d0bac9bda0f8ad2eae677bace470
Elif Su Uysal <elif.uysal@student.manchester.ac.uk>
2015-02-16 22:55:52 +0000

    Added logic for checking whether exceeded holiday limit Calendar needs to be added Commented out less than 10 drivers a day

--- src/gmpte/HolidayController.java
+++ src/gmpte/HolidayController.java
@@ -5,6 +5,10 @@
  */
 
 package gmpte;
+
+import DriverInfo;
+import java.util.Date;
+
 /**
  *
  * @author argyn
@@ -13,6 +17,55 @@ public class HolidayController {
     
     public HolidayRequestResponse holidayRequest(HolidayRequest request) {
         
-        return new HolidayRequestResponse(HolidayRequestResponse.ResponseType.NOT_GRANTED);
+        boolean isGranted = true;
+        int holidayCount = 0;
+
+        Driver driverNumber = request.getDriver();
+        Date startDate = request.getStartDate();
+        Date endDate = request.getEndDate();
+        
+        database.openBusDatabase();
+        
+        int driverID = DriverInfo.findDriver(driverNumber);
+        String driverName = DriverInfo.getName(driverID);
+        int holidaysTaken = DriverInfo.getHolidaysTaken(driverID);
+        
+        for(Date date = startDate; date.compareTo(endDate) > 0; startDate++){
+            holidayCount++;
+        }
+        
+        if(holidaysTaken + holidayCount > 25)
+            isGranted = false;
+        
+        int driversAvailable[] = new int[holidayCount];
+        
+        int[] driverIDs = DriverInfo.getDrivers();
+        
+        /* checking how many people in a given day
+           incomplete
+        for(int i = 0; i < holidayCount; i++){
+            for(int j = 0; j < driverIDs.length; j++){
+                if(driverIDs[j] != driverID){
+                    DriverInfo.isAvailable(driverIDs[j], )
+                }
+            }
+        }
+        
+        */
+        
+        
+        if(isGranted){
+            for(Date date = startDate; date.compareTo(endDate) > 0; startDate++){
+                DriverInfo.setAvailable(driverID, false, date);
+            }
+            
+            holidaysTaken += holidayCount;
+            DriverInfo.setHolidaysTaken(driverID, holidaysTaken); 
+            return new HolidayRequestResponse(HolidayRequestResponse.ResponseType.GRANTED);
+        } else{
+            return new HolidayRequestResponse(HolidayRequestResponse.ResponseType.NOT_GRANTED);
+        }
+        
+        
     }
 }
