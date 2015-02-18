85bc51cbd448d7699dfdf4f2757108e430536c5e
Elif Su Uysal <esuysal@users.noreply.github.com>
2015-02-18 22:29:08 +0000

    Added few methods to complete the logic

--- src/gmpte/holidayrequest/HolidayController.java
+++ src/gmpte/holidayrequest/HolidayController.java
@@ -9,73 +9,163 @@ package gmpte.holidayrequest;
 import gmpte.holidayrequest.HolidayRequest;
 import gmpte.databaseinterface.DriverInfo;
 import gmpte.databaseinterface.database;
+import gmpte.Driver;
 import java.util.Date;
+import java.util.Calendar;
 
 /**
  *
  * @author argyn
  */
 public class HolidayController {
+
+	/*
+     * This method counts the number of days the
+     * driver requested for holiday except for his/her
+	 * rest days
+	 */
+    public static int numberOfRequestedHolidays(Date startDate,
+                                                Date endDate,int driverID){
+
+      int holidayCount = 0;
+
+      Date currentDate = startDate;
+      Date afterEndDate = endDate;
+          
+      Calendar currentCal = Calendar.getInstance();
+      currentCal.setTime(afterEndDate);
+      currentCal.add(Calendar.DATE, 1);
+      afterEndDate = currentCal.getTime(); 
+
+      do{
+        if(DriverInfo.isAvailable(driverID, currentDate))
+          holidayCount++;
+
+        currentCal.setTime(currentDate);
+        currentCal.add(Calendar.DATE, 1);
+        currentDate = currentCal.getTime();   
+            
+      } while (currentDate.compareTo(afterEndDate) != 0);
+      return holidayCount;
+    }
+
+    /*
+     * If the requested holidays are granted
+     * this method sets those days unavailable and
+     * increments the drivers holiday count
+     */
+    public static void setDatesUnavailable(Date startDate,
+                                           Date endDate, int driverID,
+                                           int holidaysTaken){
+
+      Date currentDate = startDate;
+      Date afterEndDate = endDate;
+
+      Calendar currentCal = Calendar.getInstance();
+      currentCal.setTime(afterEndDate);
+      currentCal.add(Calendar.DATE, 1);
+      afterEndDate = currentCal.getTime(); 
+
+      do{
+
+      DriverInfo.setAvailable(driverID, currentDate, false);
+
+      holidaysTaken++;
+
+      currentCal.setTime(currentDate);
+      currentCal.add(Calendar.DATE, 1);
+      currentDate = currentCal.getTime();   
+
+      }while (currentDate.compareTo(afterEndDate) != 0);
+          
+      DriverInfo.setHolidaysTaken(driverID, holidaysTaken);
+
+	}
+
+	/*
+     * This method checks if there are enough drivers
+     * within the range that the driver requested holiday
+     * returns false if there aren't enough drivers
+	 */
+	public static boolean ifMoreThanRequired(Date startDate, Date endDate,
+                                             int driverID){
+
+      int[] driverIDs = DriverInfo.getDrivers();
+      int count;
+
+      Date currentDate = startDate;
+      Date afterEndDate = endDate;
+
+      Calendar currentCal = Calendar.getInstance();
+      currentCal.setTime(afterEndDate);
+      currentCal.add(Calendar.DATE, 1);
+      afterEndDate = currentCal.getTime(); 
+
+      do{
+        count = 0; 
+        if(DriverInfo.isAvailable(driverID, currentDate)){
+          for(int j = 0; j < driverIDs.length; j++){
+            if(driverIDs[j] != driverID){
+              if(!DriverInfo.isAvailable(driverIDs[j], currentDate))
+                count++;
+            }
+          }
+        }
+        if(count <= 10)
+        return false;
+
+        currentCal.setTime(currentDate);
+        currentCal.add(Calendar.DATE, 1);
+        currentDate = currentCal.getTime();   
+
+      }while (currentDate.compareTo(afterEndDate) != 0);
+
+      return true;
+	}	
+
+    /*
+    * This method decides whether the driver can
+	* request holidays within the given range
+    */
+	public static HolidayRequestResponse ifGranted(Date startDate,
+                                                   Date endDate,                                                       int driverID,
+                                                   boolean moreThanReqPeople,
+                                                   boolean holidayLimitExceeded,
+                                                   int holidaysTaken){
+		
+      if(moreThanReqPeople && holidayLimitExceeded){
+        
+        setDatesUnavailable(startDate, endDate, driverID, holidaysTaken);
+        return new HolidayRequestResponse(HolidayRequestResponse.ResponseType.GRANTED);
+      
+      } else
+      return new HolidayRequestResponse(HolidayRequestResponse.ResponseType.NOT_GRANTED);
+	}
     
     public HolidayRequestResponse holidayRequest(HolidayRequest request) {
        
-      /*  boolean isGranted = true;
-        int holidayCount = 0;
+        boolean holidayLimitExceeded = true;
+        boolean moreThanReqPeople = true;
+        int holidayCount;
 
         Driver theDriver = request.getDriver();
+        int driverID = theDriver.getDriverID();
         Date startDate = request.getStartDate();
         Date endDate = request.getEndDate();
         
         database.openBusDatabase();
         
-        int driverID = DriverInfo.findDriver(driverNumber);
-        String driverName = DriverInfo.getName(driverID);
         int holidaysTaken = DriverInfo.getHolidaysTaken(driverID);
-        
-        for(Date date = startDate; date.compareTo(endDate) > 0; startDate++){
-            if(DriverInfo,isAvailable(driverID, date))
-                holidayCount++;
-        }
-        
-        if(holidaysTaken + holidayCount > 25)
-            isGranted = false;
-        
-        int driversAvailable[] = new int[holidayCount];
-        
-        int[] driverIDs = DriverInfo.getDrivers();
-        
 
-        for(Date date = startDate; date.compareTo(endDate) > 0; startDate++){
-            int count = 0; 
-            if(DriverInfo,isAvailable(driverID, date)){
-                for(int j = 0; j < driverIDs.length; j++){
-                    if(driverIDs[j] != driverID){
-                        if(!DriverInfo.isAvailable(driverIDs[j], date))
-                            count++;
-                    }
-                }
-                if(count >= 10)
-                    isGranted = false;
-                
-            }
-        }
- 
-        
-        60167
-        
-        if(isGranted){
-            for(Date date = startDate; date.compareTo(endDate) > 0; startDate++){
-                DriverInfo.setAvailable(driverID, false, date);
-            }
-            
-            holidaysTaken += holidayCount;
-            DriverInfo.setHolidaysTaken(driverID, holidaysTaken); 
-            return new HolidayRequestResponse(HolidayRequestResponse.ResponseType.GRANTED);
-        } else{
-            return new HolidayRequestResponse(HolidayRequestResponse.ResponseType.NOT_GRANTED);
-       } */
-               
-    return new HolidayRequestResponse(HolidayRequestResponse.ResponseType.NOT_GRANTED);
-        
+        holidayCount = numberOfRequestedHolidays(startDate, endDate, driverID);
+
+        if(holidaysTaken + holidayCount > 25)
+          holidayLimitExceeded = false;
+
+        moreThanReqPeople = ifMoreThanRequired(startDate, endDate, driverID);
+
+        return ifGranted(startDate, endDate, driverID,
+        								 moreThanReqPeople, holidayLimitExceeded,
+        								 holidaysTaken);
     }
 }
